92676488acaf4c56c67a529c5a23a9af50668d50
0xDavidSamuel <oxdavidsamuel@aol.com>
2026-01-21 12:01:02 -0800

    Add MiladyCityGameMode.h: game mode with auth trigger

---
+++ packages/unreal/Source/MiladyCity/MiladyCityGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MiladyCityGameMode.generated.h"
+
+UCLASS(abstract)
+class AMiladyCityGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AMiladyCityGameMode();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UFUNCTION()
+    void OnAuthComplete(bool bSuccess, const FString& WalletAddress);
+};
